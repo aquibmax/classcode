d9088b3b3707fee014e03ef04e465133ad0720a5
aquibmax <mdaquib327@gmail.com>
2019-04-09 10:02:23 +0530

    Code commit

---
+++ add.js
@@ -0,0 +1,7 @@
+module.exports.add=function add(a, b){
+    return a+b;
+}
+
+module.exports = add
+
+module.exports.variable = 'abc'
---
+++ calc.js
@@ -0,0 +1,15 @@
+// const add = require('./add')
+// console.log(add(2,3))
+
+// const sub = require('./sub')
+// console.log(sub)
+
+// const div = require('./div')
+// console.log(div)
+
+// const mult = require('./mult')
+// console.log(div)
+
+const process = require('process')
+console.log(process)
+console.log(process.argv)
