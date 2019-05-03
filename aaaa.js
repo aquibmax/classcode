3ee0d5a073a0fc316743b92d4e58ff545111a72d
aquibmax <mdaquib327@gmail.com>
2019-05-03 09:53:47 +0530

    hjgkhl

--- aaaa.js
+++ aaaa.js
@@ -1,12 +1,24 @@
-var sum = 0 
-var x =1 
-var a = 0
-for(var i=0;i<10;i++){
-    console.log(a);
-    sum =a +x
-    a= x
-    x= sum
+// var sum = 0 
+// var x =1 
+// var a = 0
+// for(var i=0;i<10;i++){
+//     console.log(a);
+//     sum =a +x
+//     a= x
+//     x= sum
+// }
 
-    
-    
-}
+
+// const a = [2,3,5,9,3,4]
+// for(i=0; i<a.length; i++){
+//     var b = 0
+//     for(j=0; j<a.length; j++){
+//      if(a[i]>a[j]){
+//      b++
+//     }
+// }
+// }
+// console.log(b)
+
+
+
